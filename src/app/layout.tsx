3708b90856aae884dfda3a48ffd8689ef0760d59
sami8890 <samigabol12@gmail.com>
2025-04-29 09:10:12 +0500

    added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Navbar from "@/components/layout/navbar";
 import { useState, useEffect } from "react";
 import ContextMediaLoader from "@/app/loading"; 
+import { Analytics } from "@vercel/analytics/react"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -44,6 +45,7 @@ export default function RootLayout({
           <>
             <Navbar />
             {children}
+             <Analytics />
           </>
         )}
       </body>
